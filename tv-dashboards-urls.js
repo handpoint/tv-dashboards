d6cb259d3e609685077ca0ff548428325e29fbed
Juan Núñez <juan@handpoint.com>
2016-06-15 10:16:04 +0000

    Initial URL files

---
+++ tv-dashboards-urls.js
@@ -0,0 +1,17 @@
+/**
+ * Created by juan on 15/06/16.
+ */
+
+var Left_TV_URLS = [
+    'http://tableaulove.tumblr.com/',
+    'http://www.drawingwithnumbers.artisart.org/',
+    'http://www.vizpainter.com/',
+    'http://www.tableautinkering.wordpress.com/'
+];
+
+var Right_TV_URLS = [
+    'http://tableaulove.tumblr.com/',
+    'http://www.drawingwithnumbers.artisart.org/',
+    'http://www.vizpainter.com/',
+    'http://www.tableautinkering.wordpress.com/'
+];
