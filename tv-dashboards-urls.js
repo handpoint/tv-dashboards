22754ebe0b84a490f83cd9bae21fa0293c99c0c4
Juan Núñez <juan@handpoint.com>
2016-06-15 10:17:43 +0000

    Initial URL files

--- tv-dashboards-urls.js
+++ tv-dashboards-urls.js
@@ -3,10 +3,12 @@
  */
 
 var Left_TV_URLS = [
-    'http://tableaulove.tumblr.com/',
-    'http://www.drawingwithnumbers.artisart.org/',
-    'http://www.vizpainter.com/',
-    'http://www.tableautinkering.wordpress.com/'
+    'https://www.handpoint.com/tools/dashboards/yearly.transactions.html',
+    'https://www.handpoint.com/tools/dashboards/yearly.amount.html',
+    'https://www.handpoint.com/tools/dashboards/yearly.active.html',
+    'https://www.handpoint.com/tools/dashboards/world-map-dashboard.html',
+    'https://www.handpoint.com/tools/dashboards/monthly.transactions.html',
+    'https://www.handpoint.com/tools/dashboards/monthly.amount.html'
 ];
 
 var Right_TV_URLS = [
